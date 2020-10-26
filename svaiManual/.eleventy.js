dc237a96ed0fe5804bdde05bbbb1523d01c10faf
EvgenyDMorozov <48290365+EvgenyDMorozov@users.noreply.github.com>
2020-10-26 17:35:15 +0500

    Добавление sitemap и robots

--- svaiManual/.eleventy.js
+++ svaiManual/.eleventy.js
@@ -1,7 +1,18 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const { DateTime } = require("luxon");
+ 
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(pluginRss);
+};
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/images');
 	eleventyConfig.addPassthroughCopy('src/scripts');
 	eleventyConfig.addPassthroughCopy('src/styles');
+
+	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+	});
 
 	return {
 		dir: { input: 'src', output: '_site', data: '_data' },
